2752a2373da012391ff290963a388df84b849b0d
Rozelin <rozelin.dc@gmail.com>
2023-10-21 16:53:21 +0900

    index修正

--- webapp/sql/01_schema.sql
+++ webapp/sql/01_schema.sql
@@ -31,7 +31,7 @@ CREATE TABLE `items` (
   `created_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `updated_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
   INDEX idx_category_id (`category_id`),
-  INDEX id_created_at (`created_at` DESC, `id` DESC)
+  INDEX idx_item_select (`seller_id`, `status`, `created_at` DESC, `id` DESC)
 ) ENGINE=InnoDB DEFAULT CHARACTER SET utf8mb4;
 
 DROP TABLE IF EXISTS `transaction_evidences`;
